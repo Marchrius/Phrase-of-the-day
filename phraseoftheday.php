c1d3c89bee1630882e23b98a8750d1d60f197291
Matteo Gaggiano <maxxxx92@gmail.com>
2015-07-19 11:46:12 +0200

    License updated

--- phraseoftheday.php
+++ phraseoftheday.php
@@ -29,8 +29,9 @@ Phrase of the day: a Wordpress plugin.
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+    with this program; if not, see <https://raw.githubusercontent.com/Marchrius/Phrase-of-the-day/master/LICENSE>
+    or write to Matteo Gaggiano, <developer.marchrius [at] gmail [dot] com> .
+
 */
 define ( POD_PLUGIN_NAME , 'phraseoftheday' );
 define ( POD_PREFIX , POD_PLUGIN_NAME . '_' );
